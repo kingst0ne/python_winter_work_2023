765ed72683a7e5a202c2f1609ce1e82ebdb132ec
kingst0ne <orlov.kingstone@yandex.ru>
2023-01-18 21:26:15 +0300

    Добавлен файл с домашним заданием номер 1

---
+++ homework_lesson1.py
@@ -0,0 +1,14 @@
+#Домашнее задание 1
+
+
+x = int(input())
+y = int(input())
+
+#найти наибольшее из 5 чисел, и вывести его на экран, 2 задача - найти 2 по
+num1 = x+y
+num2 = x*y
+num3 = x**y
+num4 = x/y
+num5 = x//y
+
+print(num1,num2,num3,num4,num5)
