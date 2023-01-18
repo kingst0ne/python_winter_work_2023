d889f08e612c8d2e1442abed1ceb21af0833d849
kingst0ne <76444153+kingst0ne@users.noreply.github.com>
2023-01-18 21:04:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#import this
+import keyword
+
+
+# name = input('Как тебя зовут?')
+# # print(name)
+# surname = input('Как твоя фамилия?')
+# # print(surname)
+# print(f"Привет, {name} {surname}")
+#
+# # print(f"Привет, {s}")
+# # x = int(input("Сколько тебе лет?"))
+# # print(f"Мне {x} лет")
+#
+# name1 = input('Имя1')
+# name2 = input('Имя2')
+# name3 = input('Имя3')
+# print(f"{name3}:{name2}:{name1}")
+
+#print(keyword.kwlist)
+
+# x = int(input('chislo'))
+# if x > 0:
+#     print('positive')
+# elif x == 0:
+#     print('null')
+# else:
+#     print('negative')
+
+#
+# x = input()
+# y = input()
+# if x>y: print(x)
+# else: print(y)
+#
+# x = int(input())
+# y = int(input())
+# print(x+y, x*y)
+
+x = int(input())
+y = int(input())
+
+#На дом - найти наибольшее из 5 чисел, и вывести его на экран, 2 задача - найти 2 по
+num1 = x+y
+num2 = x*y
+num3 = x**y
+num4 = x/y
+num5 = x//y
+
+print(num1,num2,num3,num4,num5)
+
+#if num1 > num2
