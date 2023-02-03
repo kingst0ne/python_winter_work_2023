43d36e9e28ac4d2cde9c61864e043f261c0da703
kingst0ne <orlov.kingstone@yandex.ru>
2023-02-03 14:01:12 +0300

    Исправил 7.2 дз

--- Homeworks/Lesson 7/Task 7-2.py
+++ Homeworks/Lesson 7/Task 7-2.py
@@ -7,15 +7,15 @@
 def cesar_through_dict(s,t):
     dct = {}
     for i in range(ord('z') - ord('a')+1):
-        if ord('a') + t + i <= ord('z'):
-            dct[chr(i+ord('a'))] = chr(i+t+ord('a'))
+        if ord('a') + t%26 + i <= ord('z'):
+            dct[chr(i+ord('a'))] = chr(i+t%26+ord('a'))
         else:
-            dct[chr(i + ord('a'))] = chr(i + t + ord('a') - 26)
+            dct[chr(i + ord('a'))] = chr(i + t%26 + ord('a') - 26)
     for i in range(ord('Z') - ord('A')+1):
-        if ord('A') + t + i <= ord('Z'):
-            dct[chr(i + ord('A'))] = chr(i + t + ord('A'))
+        if ord('A') + t%26 + i <= ord('Z'):
+            dct[chr(i + ord('A'))] = chr(i + t%26 + ord('A'))
         else:
-            dct[chr(i + ord('A'))] = chr(i + t + ord('A') - 26)
+            dct[chr(i + ord('A'))] = chr(i + t%26 + ord('A') - 26)
 
     s1=''
     for i in s:
@@ -28,9 +28,9 @@ def cesar_through_dict(s,t):
 #Способ через цикл и 2 функции
 def check(letter, t):
     if (ord(letter) >= ord('a') and ord(letter) <= ord('z')) or (ord(letter) >= ord('A') and ord(letter) <= ord('Z')):
-        if (ord(letter)+t <= ord('z') and ord(letter) >= ord('a')) or (ord(letter)+t <= ord('Z') and ord(letter) >= ord('A')):
-            return chr(ord(letter)+t)
-        else: return chr(ord(letter) - 26 + t)
+        if (ord(letter)+t%26 <= ord('z') and ord(letter) >= ord('a')) or (ord(letter)+t%26 <= ord('Z') and ord(letter) >= ord('A')):
+            return chr(ord(letter)+t%26)
+        else: return chr(ord(letter) - 26 + t%26)
     return letter
 
 
@@ -40,7 +40,14 @@ def cesar_through_cycle(s,t):
         s_new += check(i,t)
     return s_new
 
+print(cesar_through_dict('abcABC', 53))
+print(cesar_through_cycle('abcABC', 53))
 
+print(cesar_through_dict('abcABC', 1))
+print(cesar_through_cycle('abcABC', 1))
+
+print(cesar_through_dict('abcABC', 278))
+print(cesar_through_cycle('abcABC', 278))
 
 print(cesar_through_dict('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTUVWXYZ',t=2))
 print(cesar_through_dict("It's been a hard day's night And I've been working like a dog! "
